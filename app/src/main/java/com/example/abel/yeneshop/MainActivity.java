ba7fcc85ccfdc2661341738a34b96b8b6cd786fe
Abel <abelgetahun66@gmail.com>
2024-09-28 12:54:44 +0300

    first commit

---
+++ app/src/main/java/com/example/abel/yeneshop/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.abel.yeneshop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    //variable for next page sign up delay
+    private static int Splash=4000;
+
+    // Variables for animation
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Animation for app beginning
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        // Hooks
+        image = findViewById(R.id.shoplogo);
+        logo = findViewById(R.id.firstlogname);
+        slogan = findViewById(R.id.firstmessage);
+
+        // Set animations
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        // call next activity
+        new Handler().postDelayed((new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainActivity.this,Signup.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }),Splash);
+    }
+}
