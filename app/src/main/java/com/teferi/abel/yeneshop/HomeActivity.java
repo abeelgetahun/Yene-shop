5ec1aee78adccf4f0a6afe583d0bbb35a0bd87db
Abel <abelgetahun66@gmail.com>
2024-11-01 15:11:52 +0300

    upadet on tabview

--- app/src/main/java/com/teferi/abel/yeneshop/HomeActivity.java
+++ app/src/main/java/com/teferi/abel/yeneshop/HomeActivity.java
@@ -11,11 +11,24 @@ import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.teferi.abel.yeneshop.Adapter.FragmentAdapter;
+import com.teferi.abel.yeneshop.Fragments.AddFragment;
+import com.teferi.abel.yeneshop.Fragments.ReportFragment;
+import com.teferi.abel.yeneshop.Fragments.SaleFragment;
+import com.teferi.abel.yeneshop.Fragments.StoreFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ImageButton menu_btn;
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +39,27 @@ public class HomeActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.main);
         menu_btn = findViewById(R.id.menu_button);
 
+        tabLayout=findViewById(R.id.tablayout);
+        viewPager=findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        FragmentAdapter fragmentAdapter=new FragmentAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        fragmentAdapter.addFragment(new AddFragment(),"Add");
+        fragmentAdapter.addFragment(new SaleFragment(),"Sale");
+        fragmentAdapter.addFragment(new StoreFragment(),"Store");
+        fragmentAdapter.addFragment(new ReportFragment(),"Report");
+
+        viewPager.setAdapter(fragmentAdapter);
+
+
         menu_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+
     }
 }
